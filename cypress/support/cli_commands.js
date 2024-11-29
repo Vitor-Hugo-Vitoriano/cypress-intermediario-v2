281d5a88edbab861ff37c66242997ffb55fff53d
Vitor-Hugo-Vitoriano <140897139+Vitor-Hugo-Vitoriano@users.noreply.github.com>
2024-11-28 22:06:24 -0300

    #commited

---
+++ cypress/support/cli_commands.js
@@ -0,0 +1,8 @@
+Cypress.Commands.add('cloneViaSSH', project => {
+  const domain = 'http://localhost'
+
+  cy.exec(`cd cypress/downloads/ && git clone ${domain}/${Cypress.env('user_name')}/${project.name}.git`)
+  //http://localhost:root/project-dc4ba25b-f2f8-43d6-b55b-0f0706a338ba.git
+  //http://localhost/root/project-96dd20cb-33f5-48c4-9ed5-b5b71193ab49.git
+
+})
